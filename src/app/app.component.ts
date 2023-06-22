55c7e1d3db1a856dcddb5c23ba099bad665ebf37
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-06-22 19:34:26 +0000

    Add routing using standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { WelcomeComponent } from "./welcome/welcome.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
   selector: "app-root",
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, RouterModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
