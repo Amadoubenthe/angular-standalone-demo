c61faea22eacc155f5991476cd12bb580a95adaf
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-06-22 20:18:17 +0000

    Add routing with standalone

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { DashboardComponent } from './dashboard.component';
-import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardRoutingModule } from "./dashboard-routing.module";
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [DashboardRoutingModule]
+  declarations: [],
+  imports: [DashboardRoutingModule],
 })
 export class DashboardModule {}
